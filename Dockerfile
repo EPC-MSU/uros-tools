549d4c72ac38e43fc17f8ac9fe395c20873bd170
nikatar <nikatar@disroot.org>
2021-08-27 15:59:40 +0300

    working & tested initer & flashrom functions + added func for entering into container

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 #FROM debian:bullseye-slim
-#FROM python:3.8.11-slim-bullseye
+FROM python:3.8.11-slim-bullseye
 
-COPY . /uros-tools
+COPY snapshoter/requirements.txt /uros-tools/snapshoter/requirements.txt
 WORKDIR /uros-tools
 
 #initer
 RUN apt update && apt install -y udev cpio parted fdisk mtools dosfstools
 
 #snapshoter
-#RUN apt update && apt install -y python3 pip
+RUN apt update && apt install -y python3-pip
 RUN pip install --upgrade pip && pip install -r snapshoter/requirements.txt
 
 #flashrom
+RUN apt update && apt install -y libpci3 libftdi1
--- uros-tools.sh
+++ uros-tools.sh
@@ -13,27 +13,31 @@ check_image() {
 
 initer() {
 	check_image
-	echo "$1"
-	echo "$*"
-	docker run --rm --privileged -v $PWD/"$1":$PWD/"$1" --device="$2" -ti $IMAGE /bin/bash -c "./initer/init.sh $*"
+	docker run --rm --privileged -v $PWD:/uros-tools --device="$2" -ti $IMAGE /bin/bash -c "./initer/init.sh $*"
 }
 
 snapshoter() {
 	check_image
-	docker run --rm --privileged -v $PWD/snapshoter:/uros-tools/snapshoter --device="$2" -ti $IMAGE /bin/bash -c "python3 snapshoter/snapshoter.py $*"
+	docker run --rm --privileged -v $PWD:/uros-tools --device="$2" -ti $IMAGE /bin/bash -c "python3 snapshoter/snapshoter.py $*"
 }
 
 flashrom() {
 	check_image
-	docker run --rm --privileged -ti $IMAGE /bin/bash -c "./flashrom/flashrom -p ft2232_spi:type=arm-usb-tiny,port=A,divisor=8 -w "${!#}""
-	echo -e "test flashrom is here"
+	docker run --rm --privileged -v $PWD:/uros-tools -ti $IMAGE /bin/bash -c "./flashrom/flashrom -p ft2232_spi:type=arm-usb-tiny,port=A,divisor=8 -w $*"
 }
+
+enter() {
+	check_image
+	docker run --rm --privileged -v $PWD:/uros-tools -ti $IMAGE /bin/bash
+}
+
 
 help() {
 	echo -e "\033[33m ### UROS-TOOLS WITH DOCKER ENVIRONMENT ### \033[0m"
-	echo -e "\033[32m Create working UROS storage from .swu in current folder:\033[0m ./uros-tools.sh initer /path/to/<file>.swu"
+	echo -e "\033[32m Create working UROS storage from .swu in current folder:\033[0m ./uros-tools.sh initer <file>.swu /dev/sdX"
 	echo -e "\033[32m Create .swu from UROS storage: \033[0m ./uros-tools.sh snapshoter -p <partition> -b <boot file> -s <update version> -n <name_version>"
-	echo -e "\033[32m Flash .rom to your board: \033[0m ./uros-tools.sh flashrom ЧИТАЙ"
+	echo -e "\033[32m Flash .rom to your board: \033[0m ./uros-tools.sh flashrom <file>.rom"
+	echo -e "\033[32m Enter into container for manual control: \033[0m ./uros-tools.sh enter"
 	echo -e "\033[32m Show this help page: \033[0m ./uros-tools.sh help"
 	echo -e "\033[33m ########################################### \033[0m"
 }
@@ -56,12 +60,16 @@ parse_opts() {
 			flashrom $*
 			;;
 
+		"enter")
+			enter
+			;;
+
 		"help")
 			help
 			;;
 
 		*)
-			echo -e "\n \033[31m Choose tool. See ./uros-tools.sh help \033[0m"
+			echo -e "\033[31m Choose tool. See ./uros-tools.sh help. \n You can also see each README into tool directories \033[0m"
 			exit 1
 			;;
 			
@@ -69,3 +77,4 @@ parse_opts() {
 }
 
 parse_opts $*
+
